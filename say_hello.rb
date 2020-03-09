0613cd71af5ac1d717f9c6c332b847e78e878b33
mystical-system-5659 <jennifer19hernandez96@gmail.com>
2020-03-09 22:37:41 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,9 @@
 def say_hello ("Gabriela")
   puts "Hello Gabriela"
 end
+
+it 'defaults to Ruby Programmer when no name is passed in' do
+  expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
+  say_hello("Ruby Programmer")
+end
+end
